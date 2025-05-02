9a8870db40f212fd26c660d62004524276da80b7
CarolineBrule <caroline.brule1@gmail.com>
2025-05-02 12:44:51 -0400

    Corrige problèmes d'installation liés à OneDrive

--- dev/run_tests.R
+++ dev/run_tests.R
@@ -9,3 +9,15 @@ devtools::test()
 fs::dir_tree(path = ".", depth = 2)
 
 devtools::document()
+
+# Copier le projet AquaPop vers un dossier local non synchronisé
+source_dir <- "C:/Users/bruca03/OneDrive - Ministère de l'Environnement et la Lutte contre les changements climatiques/Documents/aquapop"
+target_dir <- "C:/Users/bruca03/aqua_local/aquapop"
+
+# Crée le dossier cible s'il n'existe pas
+dir.create(dirname(target_dir), recursive = TRUE, showWarnings = FALSE)
+
+# Copie tous les fichiers
+fs::dir_copy(source_dir, target_dir, overwrite = TRUE)
+
+cat("✅ Projet copié vers :", target_dir, "\n")
